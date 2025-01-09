5a0e87113ac7f31be48201d5e417a3449758e918
SonamVernekar <sonamvernekar2003@gmail.com>
2025-01-09 19:32:18 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,13 @@
+<?php
+require_once "includes/sessions.php";
+
+ if (isset($_SESSION["lawyer_id"])) {
+ $_SESSION["lawyer_id"] = null;
+ $_SESSION["lawyer_name"] = null;
+}elseif (isset($_SESSION["client_id"])) {
+  $_SESSION["client_id"] = null;
+  $_SESSION["client_name"] = null;
+ }
+session_destroy();
+header("Location: index.php");
+
